129e290222cab760fa0cb4a91dd59c093a9589b5
Johan Frisell <johan@trell.se>
2016-11-08 11:16:56 +0100

    using publish port generator

--- deploygroup.sh
+++ deploygroup.sh
@@ -224,9 +224,9 @@ deploy_pms_group() {
     fi
 
     # create the group and check the results
-    echo "--name ${MY_GROUP_NAME} --publish ${PUBLISH_PORT} --desired ${DESIRED_INSTANCES} --min ${MIN_INSTANCES} --max ${MAX_INSTANCES} -m ${MEMORY} -e "CONTAINER_NAME=${CONTAINER_NAME}" -e "RMQ_NODE=${RMQ_NODE}" -e "RMQ_HOST=${RMQ_HOST}" -e "RMQ_PASSWORD=${RMQ_PASSWORD}" ${FULL_IMAGE_NAME}" |grep \\-\\-anti > /dev/null
+    echo "--name ${MY_GROUP_NAME} ${PUBLISH_PORT} --desired ${DESIRED_INSTANCES} --min ${MIN_INSTANCES} --max ${MAX_INSTANCES} -m ${MEMORY} -e "CONTAINER_NAME=${CONTAINER_NAME}" -e "RMQ_NODE=${RMQ_NODE}" -e "RMQ_HOST=${RMQ_HOST}" -e "RMQ_PASSWORD=${RMQ_PASSWORD}" ${FULL_IMAGE_NAME}" |grep \\-\\-anti > /dev/null
 
-    ice_retry group create --name ${MY_GROUP_NAME} --publish ${PUBLISH_PORT} --desired ${DESIRED_INSTANCES} --min ${MIN_INSTANCES} --max ${MAX_INSTANCES} -m ${MEMORY} -e "CONTAINER_NAME=${CONTAINER_NAME}" -e "RMQ_NODE=${RMQ_NODE}" -e "RMQ_HOST=${RMQ_HOST}" -e "RMQ_PASSWORD=${RMQ_PASSWORD}" ${FULL_IMAGE_NAME}
+    ice_retry group create --name ${MY_GROUP_NAME} ${PUBLISH_PORT} --desired ${DESIRED_INSTANCES} --min ${MIN_INSTANCES} --max ${MAX_INSTANCES} -m ${MEMORY} -e "CONTAINER_NAME=${CONTAINER_NAME}" -e "RMQ_NODE=${RMQ_NODE}" -e "RMQ_HOST=${RMQ_HOST}" -e "RMQ_PASSWORD=${RMQ_PASSWORD}" ${FULL_IMAGE_NAME}
 
     local RESULT=$?
 
